1c7738cb40332604458de9199978809bb338b6ac
Tanner Cottle <tcottle8@gmail.com>
2021-08-07 10:55:28 -0500

    overall style and structure / ready for redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Background from './components/Background';
+import Browse from './components/Browse';
+import Cart from './components/Cart';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Background />
 
       <Router>
+        <Navbar />
+        <Background />
         <Switch>
+          <Route exact path="/cart" component={Cart}/>
+          <Route exact path="/browse" component={Browse}/>
           <Route exact path="/" component={Home}/>
         </Switch>
       </Router>
